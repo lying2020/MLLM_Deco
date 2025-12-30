72318024496759dd1a9570896070897c381cd4d3
liying <824262075@qq.com>
2025-12-30 23:26:44 +0800

    update project.py .gitignore

---
+++ project.py
@@ -0,0 +1,10 @@
+
+
+# LML-diffusion-sampler
+import os
+
+coco_data_path = "/home/liying/Desktop/HILDA-ICLR-2025/LML-diffusion-sampler/data/coco"
+coco_annotations_path = os.path.join(coco_data_path, "annotations")
+coco_val2014_images_path = os.path.join(coco_data_path, "val2014")
+
+llava_v15_7b_path = "/home/liying/Documents/llava-v1.5-7b"
